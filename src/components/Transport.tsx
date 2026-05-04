b0db71e1645645de187a8077f8c807d91aa9cf3f
josephmyalla <chanangae@yandex.com>
2026-05-04 12:12:37 +0200

    updating transport section

--- src/components/Transport.tsx
+++ src/components/Transport.tsx
@@ -1,4 +1,5 @@
 import Heading from "./Heading"
+import { thefinestusafiri1,thefinestusafiri2,thefinestusafiri3 } from "../assets"
 const Transport =()=>{
     return(
         <section>
@@ -7,7 +8,17 @@ const Transport =()=>{
                      className="md:max-w-md lg:max-w-2xl section-title"
                      title="Transport"
                    />
-                
+                <div className="flex flex-col md:flex space-y-8 space-x-8">
+                  <div className="">
+                    <img src={thefinestusafiri1} alt="The finest Transport"/>
+                  </div>
+                  <div className="">
+                    <img src={thefinestusafiri2} alt="The finest Transport"/>
+                  </div>
+                  <div className="">
+                    <img src={thefinestusafiri3} alt="The finest Transport"/>
+                  </div>
+                </div>
             </div>
         </section>
     )
